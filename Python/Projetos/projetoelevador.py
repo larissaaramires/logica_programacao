303cf0290ba42cef2567a03600ec66569b8520cf
Larissa Ramires <larissa.souza.ramires@gmail.com>
2026-05-08 11:23:34 -0300

    Atualização

--- Python/Projetos/projetoelevador.py
+++ Python/Projetos/projetoelevador.py
@@ -3,3 +3,14 @@
 # O elevador começa no andar 0 e pode ser chamado por qualquer pessoa em qualquer andar.
 # O elevador deve se mover para o andar onde a pessoa chamou, e depois para o andar destino da pessoa.
 # O elevador deve exibir mensagens indicando o andar atual, o número de pessoas no elevador, e as ações realizadas (subindo,descendo,parando). O programa deve continuar rodando até que usúarios decida encerrar
+
+# Levantamento de Requisitos:
+# Requisitos Funcionais
+# Pode ser chamado por qualquer pessoa e em qualquer lugar
+# Tem que se mover para o andar que foi chamado e parar no andar desejado pela pessoa
+# Ter número máximo de pessoas
+# Pode ser movido para cima e para baixo
+
+# Requisitos Não Funcionais
+# Começar no andar 0
+# O elevador deve exibir mensagem indicando o andar atual, o número de pessoas no nele e as acões realizadas (subindo, descendo, parando)
