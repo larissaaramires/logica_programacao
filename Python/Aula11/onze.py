c844009292ad5d3db70c76abcf6474dad7e9adba
Larissa Ramires <larissa.souza.ramires@gmail.com>
2026-05-15 10:36:04 -0300

    Atualização Aula 11

--- Python/Aula11/onze.py
+++ Python/Aula11/onze.py
@@ -1,30 +1,22 @@
 # Explicação de def: A palavra-chave "def" é usada para definir uma função em Python. Uma função é um bloco de código reutilizável que realiza uma tarefa específica.
 # return: A palavra-chave "return" é usada para finalizar a execução de uma função e retornar um valor para o local onde a função foi chamada. O valor retornado pode ser usado posteriormente no código.
 
-# def nome_da_funcao(parametro1, parametro2):
-#     # Corpo da função (código que será executado)
-#     resultado = parametro1 + parametro2
-#     return resultado
+def nome():
+    nome = input("Digite seu nome: ")
+    return nome
+print(f"Olá, {nome()}!")
 
-# def saudacao(nome):
-#     return f"Olá, {nome}!"
+def valores():
+    print("Digite três valores:")
+    a = int(input("Digite o primeiro valor: "))
+    b = int(input("Digite o segundo valor: "))
+    c = int(input("Digite o terceiro valor: "))
+    return a, b, c
+print(f"O maior valor é: {max(valores())}") 
 
-# def nome():
-#     nome = input("Digite seu nome: ")
-#     return nome
-# print(f"Olá, {nome()}!")
-
-# def valores():
-#     print("Digite três valores:")
-#     a = int(input("Digite o primeiro valor: "))
-#     b = int(input("Digite o segundo valor: "))
-#     c = int(input("Digite o terceiro valor: "))
-#     return a, b, c
-# print(f"O maior valor é: {max(valores())}") 
-
-# # Reutilizando funções
-# nome()
-# valores()
+# Reutilizando funções
+nome()
+valores()
 
 ## Conceitos Chave
 # def: Indica o início da definição da função.
